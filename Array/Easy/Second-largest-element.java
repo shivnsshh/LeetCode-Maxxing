b9d2880c9d38691b9737ea6e62de5c22523eea85
Shivansh <132641847+shivnsshh@users.noreply.github.com>
2026-03-31 12:55:58 +0530

    Create Second-largest-element.java

---
+++ Array/Easy/Second-largest-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int max = -1, second = -1;
+
+        for (int num : arr) {
+            if (num > max) {
+                second = max;
+                max = num;
+            } 
+            else if (num < max && num > second) {
+                second = num;
+            }
+        }
+
+        return second;
+    }
+}
