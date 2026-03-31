ff8f4b616a607198de6f128fefe1d0db3032e3af
Shivansh <132641847+shivnsshh@users.noreply.github.com>
2026-03-31 21:58:01 +0530

    Create 10)-Find-single-number.java

---
+++ Array/Easy/10)-Find-single-number.java
@@ -0,0 +1,15 @@
+class Solution
+{
+    public int singleNumber(int[] nums) 
+    {
+        int c=0;
+
+        for(int i=0; i<nums.length; i++)
+        {
+            c^=nums[i];
+        }
+        return c;
+    }  
+}
+
+//https://leetcode.com/problems/single-number/?utm=codolio
