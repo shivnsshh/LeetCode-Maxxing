67906309f12f90d075ff3df9c49c999b4703a138
Shivansh <132641847+shivnsshh@users.noreply.github.com>
2026-04-14 22:44:17 +0530

    Create 01)-Remove-outer-parenthesis.java

---
+++ String/Easy/01)-Remove-outer-parenthesis.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        StringBuilder res = new StringBuilder();
+        int depth = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                if (depth > 0) res.append(c);
+                depth++;
+            } else {
+                depth--;
+                if (depth > 0) res.append(c);
+            }
+        }
+
+        return res.toString();
+    }
+}
+
+//https://leetcode.com/problems/remove-outermost-parentheses/?utm=codolio
